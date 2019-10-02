6404860c003054eb552a5b7b5b4a6300be1fb138
CelmiraS <56045357+CelmiraS@users.noreply.github.com>
2019-10-02 16:32:21 +0100

    Add files via upload
    Login

---
+++ LoginClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Chrome;
+using Nest;
+
+namespace LoginV2_NUnitTestProject
+{
+    // login class that interact with elements of the login page
+    class LoginClass
+    {
+
+        // fill the username field
+        public void FillUsername(IWebDriver driver , string username )
+        {
+            driver.FindElement(By.Id("username")).SendKeys(username);
+        }
+
+        // fill the password field
+        public void FillPassword(IWebDriver driver, string password)
+        {
+            driver.FindElement(By.Id("password")).SendKeys(password);
+        }
+
+        // method to click on login button
+        public void SubmitCredentials(IWebDriver driver)
+        {
+            driver.FindElement(By.CssSelector("#login > button > i")).Click();
+        }
+
+        // method to click on logout button
+        public void Logout (IWebDriver driver)
+        {
+            driver.FindElement(By.CssSelector("#content > div > a")).Click();
+        }
+
+        // method to obtain the message generated
+        public string  GetMessage (IWebDriver driver)
+        {
+            IWebElement messageField = driver.FindElement(By.Id("flash-messages"));
+            string messageContent = messageField.Text.ToString();
+            return messageContent;
+        }
+
+
+        //implicit wait method
+        public void Wait(IWebDriver driver, int time)
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(time);
+
+        }
+
+        // method to close the browser
+        public void CloseDriver(IWebDriver driver)
+        {
+            Wait(driver, 9000);
+            driver.Close();
+        }
+    }
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -3,50 +3,90 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Threading;
 
-
-namespace KeyPresses_NUnitTestProject
+namespace LoginV2_NUnitTestProject
 {
+    //class of the testcases described on the document
+    // Each testcase represents scenario
     public class Tests
     {
-
         IWebDriver driver;
-        KeyPressesClass keyClass;
+        string username = "tomsmith";
+        string password = "SuperSecretPassword!";
 
         //method that run before each testcase (to alow open the browser)
-        [OneTimeSetUp]
+        [SetUp]
         public void Setup()
         {
-
             driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/key_presses");
-    
-
+            driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/login");
         }
 
-        // Simulate when the user fill the key and according the keyboard dictionary return True, if in all iterations the keys submeted and expected are the same
+        // Possible scenario to generate a submition of credentials with a invalid username
         [Test(), Order(1)]
-        public void isSameKey()
+        public void IsincorrectUsername()
         {
-            keyClass = new KeyPressesClass();
+            string incorrectUsername = username.ToUpper();
+            string messageObtained = "";
 
-            //Repeat the request  4 times
-            int round = 0;
-            bool isAlwaysSameKey = true;
-            while (round<5)
-            {
-                round = round + 1;
+            LoginClass loginClass = new LoginClass();
+            loginClass.FillUsername(driver, incorrectUsername);
+            loginClass.FillPassword(driver, password);
+            loginClass.Wait(driver, 3000);
+            loginClass.SubmitCredentials(driver);
+            Thread.Sleep(2000);
+            messageObtained = loginClass.GetMessage(driver);
+            Assert.IsTrue((messageObtained.Contains("Your username is invalid!")));
+            loginClass.CloseDriver(driver);
+         }
 
-                bool isvalid = keyClass.CompareKeys(driver);
-                Thread.Sleep(2000);
-                if (isvalid.Equals(false))
-                    isAlwaysSameKey = false;
-            
-            }
-            Assert.IsTrue(isAlwaysSameKey);
+        // Possible scenario to generate a submition of credentials with a invalid password
 
-            driver.Close();
+        [Test(), Order(2)]
+        public void IsincorrectPassword()
+        {
+            string incorrectPassword = password.ToLower();
+            string messageObtained = "";
+
+            LoginClass loginClass = new LoginClass();
+            loginClass.FillUsername(driver,username );
+            loginClass.FillPassword(driver, incorrectPassword);
+            loginClass.Wait(driver, 3000);
+            loginClass.SubmitCredentials(driver);
+            Thread.Sleep(2000);
+            messageObtained = loginClass.GetMessage(driver);
+            Assert.IsTrue((messageObtained.Contains("Your password is invalid!")));
+            loginClass.CloseDriver(driver);
         }
 
-      
+
+        // Scenario to generate a submition of valid credentials and then a logout process
+
+       [Test(), Order(3)]
+        public void IsexecutedLoginAndLogout()
+        {
+            
+            string messageObtained = "";
+            string messageObtained2 = "";
+
+            LoginClass loginClass = new LoginClass();
+            loginClass.FillUsername(driver, username);
+            loginClass.FillPassword(driver, password);
+            loginClass.Wait(driver, 3000);
+            loginClass.SubmitCredentials(driver);
+            Thread.Sleep(2000);
+            messageObtained = loginClass.GetMessage(driver);
+            Thread.Sleep(5000);
+            loginClass.Logout(driver);
+            loginClass.Wait(driver,2000);
+            messageObtained2 = loginClass.GetMessage(driver);
+            Thread.Sleep(2000);
+            Assert.IsTrue((messageObtained.Contains("You logged into a secure area!")) && messageObtained2.Contains("You logged out of the secure area!"));
+
+
+            loginClass.CloseDriver(driver);
+        }
+
+
+
     }
 }
